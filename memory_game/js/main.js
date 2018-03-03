0c450e13b6e01382ef4bc3746b5f9d280b851c81
Michael Slocum <mslocum819@gmail.com>
2018-03-03 00:26:50 -0800

    Improved game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,8 @@
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0]
-cardsInPlay.push(cardOne);
-console.log("Up and running!");
-console.log("User flipped " + cardsInPlay);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
 
+var checkForMatch = function(){
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0]===cardsInPlay[1]) {
 alert("You found a match!");
@@ -16,3 +10,17 @@ alert("You found a match!");
 alert("Sorry, try again.");
 	}
 }
+}
+
+var flipCard = function(cardId){
+console.log("user flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
