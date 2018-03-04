ade30dae28d423513bde94d9d7f46ee0b01b2ea0
Michael Slocum <mslocum819@gmail.com>
2018-03-03 21:54:45 -0800

    Made Cards and object array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,28 @@
 
-var cards = ["queen","queen","king","king"];
+var cards = 
+	[
+{
+	"rank" :  "queen",
+	"suit" : "hearts",
+	"cardImage" : "images/queen-of-hearts.png"
+},
+{
+	"rank" :  "queen",
+	"suit" : "diamonds",
+	"cardImage" : "images/queen-of-diamonds.png"
+},
+{
+	"rank" :  "king",
+	"suit" : "hearts",
+	"cardImage" : "images/king-of-hearts.png"
+},
+{
+	"rank" :  "king",
+	"suit" : "diamonds",
+	"cardImage" : "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -13,14 +36,15 @@ alert("Sorry, try again.");
 }
 
 var flipCard = function(cardId){
-console.log("user flipped " + cards[cardId]);
+console.log("user flipped " + cards[cardId].rank);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
 }
 
 flipCard(0);
 flipCard(2);
-
-
 
